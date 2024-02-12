5f1eca0fafb4ee1135075ffc6c9d62e51fa485c1
Ada Pici <55006272+adamaripici@users.noreply.github.com>
2024-02-12 15:52:51 -0600

    autoscaling

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
-  vpc_zone_identifier = module.bloc_vpc.public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
@@ -109,7 +109,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id = aws_instance.blog.id
+    
     }
   }
 
